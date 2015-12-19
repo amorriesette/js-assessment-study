247b0be4ff1ba091a6d3852947d3214fe28dad3c
gelifaith <Angelica@Frosty.local>
2015-12-19 01:52:18 -0600

    Added partial Array.js

--- app/arrays.js
+++ app/arrays.js
@@ -3,10 +3,13 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
+    //'you should be able to determine the location of an item in an array'
+    return arr.indexOf(item);
 
   },
 
   sum : function(arr) {
+    //'you should be able to add the values of an array'
 
   },
 
@@ -15,6 +18,7 @@ exports.arraysAnswers = {
   },
 
   removeWithoutCopy : function(arr, item) {
+    //you should be able to remove all instances of a value from an array, returning the original array'
 
   },
 
@@ -55,6 +59,17 @@ exports.arraysAnswers = {
   },
 
   findAllOccurrences : function(arr, target) {
+    //you should be able to find all occurrences of an item in an array
+    //console.log(arr, ", target: ", target);
+    //var i = 0, total = 0;
+    //console.log(arr.length);
+    //while(i != arr.length){
+      //if(arr[i] == target){
+        //here[total] = i;
+        //console.log(i);
+      //}
+    //}
 
+    //console.log("locations of target: " +  here.length);
   }
 };
